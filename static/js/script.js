6fc02d2842da53486af665400c97990441ccb1b4
samuel21695 <samuel21695@gmail.com>
2023-10-26 17:45:37 +0900

    #3 Map API 가져오기 Fixes #3

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+// script.js
+var mapOptions = {
+  center: new naver.maps.LatLng(37.5666102, 126.9783881), // 서울의 위도, 경도
+  zoom: 10 // 초기 확대 레벨
+};
+
+var map = new naver.maps.Map('map', mapOptions);
+
+// 지도에 마커 추가 예제
+var marker = new naver.maps.Marker({
+  position: new naver.maps.LatLng(37.5666102, 126.9783881),
+  map: map
+});
