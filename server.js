d64cf39e58a448876a5682d091844563157df436
Yusmin97 <tmdamsi97@gmail.com>
2023-10-26 16:55:03 +0900

    static 폴더와 하위 폴더 정적 파일을 제공

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const path = require('path');
 
 const app = express();
 const port = 3000;
+
+app.use('/static',express.static(path.join(__dirname, 'static')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
