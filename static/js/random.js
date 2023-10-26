25590a2647eb64626e6d753b0a5a412064ba1310
Yusmin97 <tmdamsi97@gmail.com>
2023-10-26 18:05:40 +0900

    랜덤버튼 기능 js파일 작성

---
+++ static/js/random.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let map = new naver.maps.Map('map', {
+    center: new naver.maps.LatLng(37.5666102, 126.9783881),
+    zoom: 10
+  })
+})
+
+function moveToRandomLocation() {
+  
+}
