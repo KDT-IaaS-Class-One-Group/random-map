8b080e0465a4b1e05038c48b2a04e209a6693642
Yusmin97 <tmdamsi97@gmail.com>
2023-11-01 12:04:37 +0900

    도시별 이름 위도,경도 배열로 생성

---
+++ static/js/citiesArray.js
@@ -0,0 +1,22 @@
+const cities = [
+  {name: 'Seoul', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Daejeon', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Busan', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Daegoo', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Incheon', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Gwangjoo', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Ulsan', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Sejong', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Chuncheon', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Pohang', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Changwon', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Jeonju', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Mokpo', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Cheongju', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Cheonan', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'Jeju', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+  {name: 'South-Korea', center:{lat: 37.5666102, lng: 126.9783881}, northEast: {lat: 37.701857, lng: 127.148242}, southWest: {lat: 37.428725, lng:126.767625}},
+];
+
+
+module.exports = cities;
