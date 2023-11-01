431f3fd3523cd8a265bbab36822ed9487f25bcdb
samuel21695 <samuel21695@gmail.com>
2023-11-01 10:57:56 +0900

    🧪 test: Map ApI 동적으로 로드

--- static/js/mapClientId.js
+++ static/js/mapClientId.js
@@ -1,4 +1,10 @@
 // 환경변수에서 MAP_CLIENT_ID 값 가져오기
-const apiKey = process.env.MAP_CLIENT_ID;
+const mapClientId = process.env.MAP_CLIENT_ID;
 
-console.log(apiKey); 
+console.log(mapClientId); 
+
+// Naver Maps API 동적으로 로드하기
+const script = document.createElement('script');
+script.src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${mapClientID}`;
+script.async = true;
+document.head.appendChild(script);
