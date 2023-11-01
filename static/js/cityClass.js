b967b219e9b2b59b941e0c95f7432f583fd9ab54
Yusmin97 <tmdamsi97@gmail.com>
2023-11-01 14:40:47 +0900

    cities 생성자 함수 ESM 방식 모듈화

--- static/js/cityClass.js
+++ static/js/cityClass.js
@@ -1,11 +1,11 @@
-class City {
+export default class City {
   constructor(name, center, northEast, southWest) {
     this.name = name;
-    this.center = { lat: center.lat, lng: center.lng };
+    // this.center = new naver.maps.LatLng{ center.lat, center.lng };
+    this.center = new naver.maps.LatLng(center.lat, center.lng);
     this.northEast = { lat: northEast.lat, lng: northEast.lng };
     this.southWest = { lat: southWest.lat, lng: southWest.lng };
   }
 }
 
-module.exports = City;
 
