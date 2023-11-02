832d0c9536af8231bd8a2fd9015a5cc4ca90adb2
Yusmin97 <tmdamsi97@gmail.com>
2023-11-02 09:35:44 +0900

    모듈화 한 생성자 함수를 사용한 코드작성(작동은 x) 실험 단계

--- static/js/script.js
+++ static/js/script.js
@@ -4,38 +4,43 @@ import City from './cityClass.js';
 document.addEventListener('DOMContentLoaded', () => {
   // 도시 정보를 객체 배열로 정의
   let currentCityIndex = 0;
+  let map;
 
-  citiesArray.forEach((cityData) => {
+  citiesArray.forEach((cityData)=> {
+    const newCity = new City(cityData.name, cityData.center, cityData.northEast, cityData.southWest);
 
-    let map = new naver.maps.Map('map', {
-      center: cityData.center,
-      zoom: 15,
-    });
-
+    if (!map) {
+      map = new naver.maps.Map('map', {
+        center: newCity.center,
+        zoom: 15,
+      });
+    }
     // 랜덤한 위치로 이동하는 함수
     function moveToRandomLocation() {
       const newLat = cityData.southWest.lat + Math.random() * (cityData.northEast.lat - cityData.southWest.lat);
       const newLng = cityData.southWest.lng + Math.random() * (cityData.northEast.lng - cityData.southWest.lng);
       const newLocation = new naver.maps.LatLng(newLat, newLng);
-
+  
       map.panTo(newLocation);
     }
-
+  
     const randomLocationBtn = document.getElementById('randomLocationBtn');
-
+  
     // 버튼 클릭 시 랜덤 위치로 이동 함수 호출
     randomLocationBtn.addEventListener('click', moveToRandomLocation);
-
+  
     // 다음 도시로 이동하는 함수
     function moveToNextCity() {
       currentCityIndex = (currentCityIndex + 1) % citiesArray.length;
-      const nextCity = citiesArray[currentCityIndex];
+      const nextCityData = citiesArray[currentCityIndex];
+      const nextCity = new City(nextCityData.name, nextCityData.center, nextCityData.northEast, nextCityData.southWest);
       map.setCenter(nextCity.center);
     }
-
+  
     const nextCityBtn = document.getElementById('nextCityBtn');
-
+  
     // 버튼 클릭 시 다음 도시로 이동 함수 호출
     nextCityBtn.addEventListener('click', moveToNextCity);
-  });
+  })
+
 });
